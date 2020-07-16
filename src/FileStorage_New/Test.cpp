8831d2ea1e4539af7a2a99902209ef302a89643e
lyw414 <347528962@qq.com>
2020-07-16 18:13:05 +0800

    add filestorage IndexFile cache logic

--- src/FileStorage_New/Test.cpp
+++ src/FileStorage_New/Test.cpp
@@ -4,12 +4,13 @@
 int main()
 {
     LYW_CODE::FileStorage<> storage;
+    printf("%d\n",sizeof(LYW_CODE::FileStorage<>::TIndexFileBlock));
     char buf[] = "12345678";
     char buf2[100] = {0};
-    void * p = storage.allocate(100);
+    unsigned int p = storage.allocate(100);
     storage.write(p, buf, 8);
-    storage.read(p,buf2,100);
-    printf("%s\n", buf2);
+    int ret = storage.read(p,buf2,100);
+    printf("%d %s\n",ret, buf2);
     storage.free(p);
 
     return 0;
